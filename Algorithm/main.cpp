ffaffbdc8cecf04364f55827e485ecf25a51e0ee
Human-developer <liouvnet0705@naver.com>
2021-12-16 12:00:39 +0900

    Initial Commit

---
+++ Algorithm/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Algorithm
+//
+//  Created by 금성 on 2021/12/16.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
